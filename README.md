b1c67493277c9bb72ef6912afcfc646209080fd7
Sebastian Plaza <sebastianplaza@gmail.com>
2022-02-04 21:07:11 +0100

    feat: tmux config install

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # dotfiles
+
+First, install this config of tmux
+
+https://github.com/gpakosz/.tmux
+
+```bash
+cd
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+```
--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Clone repo
+
 git clone https://github.com/sebaplaza/dotfiles.git ~/dotfiles
 
 # make symbolic links
 
 cd ~
-ln -s .gitconfig ~/dotfiles/.gitconfig
-ln -s .tmux.conf ~/dotfiles/.tmux.conf
+ln -s -f ~/dotfiles/.gitconfig
+ln -s -f ~/dotfiles/.tmux.conf.local
 
 
 echo Done!
